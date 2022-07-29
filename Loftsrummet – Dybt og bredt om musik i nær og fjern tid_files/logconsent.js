24136c47063b55020e61b969e40afeef4314a012
nielsgreniman <105637299+nielsgreniman@users.noreply.github.com>
2022-07-29 21:39:31 +0200

    Add files via upload
    loftsrummet added

---
+++ Loftsrummet – Dybt og bredt om musik i nær og fjern tid_files/logconsent.js
@@ -0,0 +1,5 @@
+CookieConsent.setCookie("{stamp:%27+QgF+o8tlGvfI7aOXUWOgdsWY64mxq7j1UEWPtIcGq9/bW+a/VpEVw==%27%2Cnecessary:true%2Cpreferences:false%2Cstatistics:false%2Cmarketing:false%2Cver:1%2Cutc:1659123420142%2Cregion:%27dk%27}", new Date(1690659420142), '/', '', '');
+if (typeof CookieConsent.removeBulkReset!='undefined') {CookieConsent.removeBulkReset();}
+CookieConsent.userCountry = 'dk';
+CookieConsent.init();
+if (typeof CookieConsent.resetCookies!='undefined') {CookieConsent.resetCookies();}
